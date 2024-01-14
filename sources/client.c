06620be79c69bd10a24a299dcf17bf48dd68fa77
Adedayo Sanni <asanni@student.42sp.org.br>
2024-01-13 21:37:53 -0300

    add comentários no meu projeto

--- sources/client.c
+++ sources/client.c
@@ -6,11 +6,25 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:39:41 by asanni            #+#    #+#             */
-/*   Updated: 2024/01/13 19:19:34 by asanni           ###   ########.fr       */
+/*   Updated: 2024/01/13 21:31:31 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
+
+// a variavel global apos cada char (8 bits) para ela poder 
+// enviar de nova elaestá funcionado como um "index"
+// pq uma var global ao inves de um index normal????
+//	pq ela vai ser resetada apos 8 rotações(deps de imprimir um char) 
+//	das funçoes enquanto que um index a cada 1 passagem, não completando o byte
+
+int	g_send_bit = 0;
+
+void	send_signal(int signal)
+{
+	if (signal == SIGUSR1)
+		g_send_sinal = 1;
+}
 
 //Função que da free na msg_copy printa msg de erro e da exit
 void	error(char *msg)
@@ -19,7 +33,6 @@ void	error(char *msg)
 	ft_printf("Error:\nFail to communicate");
 	//encerrar communicação
 	exit(EXIT_FAILURE);
-	
 }
 
 int	verif_msg(int argc, char **argv)
@@ -51,14 +64,13 @@ void	send_msg(int pid, char msg)
 	if (msg)
 		msg_copy = ft_strdup(msg);
 	if (!msg_copy)
-		error (0); 
+		error (0);
 	if (pid)
 		server_pid = pid;
 	if (msg_copy[++bit / 8])
 	{
 		/* code */
 	}
-	
 }
 
 	// i = 7;
@@ -76,16 +88,25 @@ void	send_msg(int pid, char msg)
 //criar o main para enviar a msg
 int	main(int argc, char **argv)
 {
-	int	msg_check;
+	struct sigaction	sa;
+	int					msg_check;
+	int					pid_num;
+	int					i;
 
+	i = 0;
 	msg_check = verif_msg(argc, argv);
 	if (msg_check == 1)
 	{
-	/* 	signal(SIGUSR1, handler_sigusr);//TODO funcao para manipular sinais
-		signal(SIGUSR2, handler_sigusr); */
-		send_msg(ft_atoi(argv[1]), argv[2][0]);
+		pid_num = ft_atoi(argv[1]);
+		sa.sa_handler = send_signal;
+		sa.sa_flags = 0;
+		sigaction(SIGUSR1, &sa, NULL);
+		while (argv[2][i] != '\0')
+		{
+			//send_msg(pid_num, argv[2][i]);
+			i++;
+		}
+		ft_printf('a');
 	}
-	else
-		exit(EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+	return (0);
 }
--- sources/server.c
+++ sources/server.c
@@ -6,31 +6,34 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 18:06:24 by asanni            #+#    #+#             */
-/*   Updated: 2024/01/13 19:19:46 by asanni           ###   ########.fr       */
+/*   Updated: 2024/01/13 21:37:28 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-//criar função para imprimir mensagem
-void	print_message(int msg)
-{
-}
+//criar função para receber e imprimir a mensagem do client
+//elá vai receber 1 bit por por vez do client ate formar um char
+//depois que receber 1 byte(8 bits) ela imprime o char e vai 
+//retornar a comunicação pro client para saber qual sera a nova 
+//sequencia de bits a receber
 
-int	sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
-{
-	struct sigaction
-	{
-		void (*sa_sigaction)(int, siginfo_t *, void *);
-		sigset_t sa_mask;
-		int sa_flags;
-	}
+//a função para receber/imprimir o bit vai funcionar igual a print num
+// o sinal vai literalmente encviar 1 e 0
 
+
+//estudar communicação com binarios em char
+int	g_receive_bit = 0;
+
+static void	print_msg(int signal, siginfo_t *siginfo, void *context)
+{
+	//context é void - não vou usar tenho que castar o void
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	struct sigaction	sa;
+	int					pid;
 
 	if (argc != 1)
 		ft_printf("Error:\nThe input should be as follow:./server");
@@ -38,7 +41,22 @@ int	main(int argc, char **argv)
 	{
 		pid = getpid();
 		ft_printf("Server PID: %i", pid);
-		//signal(SIGUSR1, print_message);
+		sa.sa_sigaction = print_msg;
+		sa.sa_flags = SA_SIGINFO;
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		while(1)
+			pause();
 	}
 	return (0);
 }
+
+//sa_handler specifies the action to be associated with signum and 
+//may be SIG_DFL for the default action, 
+//SIG_IGN to ignore this signal, or a pointer to a signal handling function.  
+//This function receives the signal number as its only argument.
+// diff entre sa handler and sa sigaction é a qntd de info que eu quero
+// ou preciso passar, a sigaction passa o num do sinal, as flas e etc..
+// a handler passa só o int do sinal.
+
+//SA_SIGINFO - centraliza as infos do sinal
